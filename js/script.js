7e59209c8814a245d7d15e471239829021592482
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-25 23:38:47 +0200

    [UPD] variable price

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
 const distance_km = prompt ('Numero di chilometri ?')
 const age = prompt ('Età del passeggero')
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const price = distance_km * 0.21;
