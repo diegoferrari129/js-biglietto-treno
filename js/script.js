2890ee7df60b09ac81145bb942fc712264da3882
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-25 23:27:57 +0200

    [FEAT] variable distance_km and age

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+const distance_km = prompt ('Numero di chilometri ?')
+const age = prompt ('Età del passeggero')
