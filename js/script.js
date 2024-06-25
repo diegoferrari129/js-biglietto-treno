eaea85bffeb3d91e88f2ad18acc7370807885861
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-25 23:47:42 +0200

    [UPD] discount variables

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const age = prompt ('Età del passeggero')
 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 const price = distance_km * 0.21;
+
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65
+let discount = 0
+
+if (age < 18) {
+    discount = 20%
+}
+
+else if (age > 65)
+    discount = 40%
